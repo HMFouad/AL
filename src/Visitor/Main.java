65bbcc8abd936a190006d6ea887312c167f4496f
HMFouad <HMFouad@Yahoo.FR>
2017-04-25 10:48:04 +0200

    Add Visitor

---
+++ src/Visitor/Main.java
@@ -0,0 +1,11 @@
+package Visitor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ComputerPart computer = new Computer();
+		computer.accept(new ComputerPartDisplayVisitor());
+
+	}
+
+}
---
+++ src/Visitor/Monitor.java
@@ -0,0 +1,11 @@
+package Visitor;
+
+public class Monitor implements ComputerPart {
+
+	@Override
+	public void accept(ComputerPartVisitor computerPartVisitor) {
+		computerPartVisitor.visit(this);
+
+	}
+
+}
