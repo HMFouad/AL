db8af9ac7ac461aee54805f593256ff604f69727
HMFouad <HMFouad@Yahoo.FR>
2017-04-25 09:51:51 +0200

    Add Facade

---
+++ src/Facade/Main.java
@@ -0,0 +1,14 @@
+package Facade;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ShapeMaker shapeMaker = new ShapeMaker();
+		
+		shapeMaker.drawRectangle();
+		shapeMaker.drawSquare();
+		shapeMaker.drawCircle();
+
+	}
+
+}
